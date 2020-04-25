9b011eda63bd7208e03fa2869832d6f1f591cfdb
sivatorov <sivatorov@gmail.com>
2020-04-25 17:08:49 +0300

    init commit

---
+++ app/src/main/java/ru/sivatorov/moneytracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.sivatorov.moneytracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
